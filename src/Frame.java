4cce1180df133591bdee1def746bb1037d32a347
somename <someemail>
2017-05-12 12:04:59 +0200

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -34,7 +34,12 @@ public class Frame {
 	
 	//return whether a frame is a spare or not
 	public boolean isSpare(){
-		//to be implemented
+		if(firstThrow + secondThrow == 10)
+		{
+			return true;
+		}
+		else
+			
 		return false;
 	}
 }
