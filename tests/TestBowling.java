73e0c4de4a5f1fcebab4189f54a3f90c4e8407c2
somename <someemail>
2017-05-12 12:25:22 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -11,37 +11,37 @@ public class TestBowling {
 	}
 	
 	@Test
-	public void test_isNotFirstThrowStrike() {
+	public void test_isNotFirstThrowStrike() throws BowlingException {
 		Frame fr = new Frame(2, 0);
 		assertFalse(fr.isStrike());
 	}
 	
 	@Test
-	public void test_isSpare() {
+	public void test_isSpare() throws BowlingException {
 		Frame fr = new Frame(5, 5);
 		assertTrue(fr.isSpare());
 	}
 	
 	@Test
-	public void test_isNotSpare() {
+	public void test_isNotSpare() throws BowlingException {
 		Frame fr = new Frame(5, 3);
 		assertFalse(fr.isSpare());
 	}
 	
 	@Test
-	public void test_isScore() {
+	public void test_isScore() throws BowlingException {
 		Frame fr = new Frame(5, 3);
 		assertEquals("Is score: ", 8, fr.score());
 	}
 	
 	@Test
-	public void test_isNotScore() {
+	public void test_isNotScore() throws BowlingException {
 		Frame fr = new Frame(5, 3);
 		assertNotEquals("Is not score: ", 9, fr.score());
 	}
 	
 	@Test
-	public void test_isFrameAdded() {
+	public void test_isFrameAdded() throws BowlingException {
 		BowlingGame game = new BowlingGame();
 		Frame fr = new Frame(5, 3);
 		game.addFrame(fr);
